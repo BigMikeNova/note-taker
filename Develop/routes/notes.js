1f892885a6ac37e142e08538789be188fa662dbf
BigMikeNova <mike.novachek@gmail.com>
2023-07-27 21:39:27 -0400

    did the whole thing yo

---
+++ Develop/routes/notes.js
@@ -0,0 +1,56 @@
+const fb = require('express').Router();
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
+
+// GET Route for retrieving all the notes
+fb.get('/', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new note
+fb.post('/', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error in posting note');
+    }
+});
+
+// DELETE Route for a specific note
+fb.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    console.log(noteId);
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            // Make a new array of all notes except the one with the ID provided in the URL
+            const result = json.filter((note) => note.id !== noteId);
+
+            // Save that array to the filesystem
+            writeToFile('./db/db.json', result);
+
+            // Respond to the DELETE request
+            res.json(`Item ${noteId} has been deleted 🗑️`);
+        });
+});
+
+module.exports = fb;
